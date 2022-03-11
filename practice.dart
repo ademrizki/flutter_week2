b0097871d55bfb3826406c0cfa13c8141c00e84c
ademrizki <ade.rizky@amartha.com>
2022-03-11 18:30:56 +0700

    Add comments

---
+++ practice.dart
@@ -0,0 +1,19 @@
+void main() async {}
+
+/// Kelas biasa
+
+/// Abstract class
+
+/// Constructor
+
+/// Inheritence
+
+/// Overriding
+
+/// Interface (Inheritence with implements, all required)
+
+/// Polymorphism
+
+/// Generics class
+
+/// Generics function
